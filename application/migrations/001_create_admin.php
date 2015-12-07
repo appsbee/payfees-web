2c3cfc09ef04f4ab283ff0510f3459d7639944da
Ratul Ghosh <ratul.ghosh@appsbee.com>
2015-12-07 16:13:23 +0530

    # Migration added to create admin table  # Migration controller added to made auto-migrate easy

---
+++ application/migrations/001_create_admin.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_Admin extends CI_Migration
+{
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'full_name' => array(
+                'type' => 'TEXT',
+                'null' => TRUE,
+            ),
+            'password' => array(
+                'type' => 'TEXT',
+                'null' => TRUE,
+            ),
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('admins');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('admins');
+    }
+
+}
+
+?>
